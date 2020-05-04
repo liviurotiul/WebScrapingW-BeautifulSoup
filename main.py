a891ff310a3bef28939c8da5944174c8b24608a4
liviurotiul <liviurotiultelefon@gmail.com>
2020-05-04 19:53:15 +0300

    working first few lines of code

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ A quick little web scraping project that notifies if there are any new in the el
 
 import os
 import bs4
-from urllib.request import urlopen
+from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
+# the page(s) we are gonna be scraping
+site_url = 'https://www.youtube.com/results?search_query=the+elder+scrolls+6&sp=CAMSBAgDEAE%253D'
+
+# get the page and read the html
+uClient = uReq(site_url)
+page_html = uClient.read()
+uClient.close()
+
+# parse the hmtl
+page_soup = soup(page_html, "html.parser")
