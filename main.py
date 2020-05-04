bf98d9ebd8e668e2188db3a3b586d66f667619f2
liviurotiul <liviurotiultelefon@gmail.com>
2020-05-04 19:35:04 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+A quick little web scraping project that notifies if there are any new in the elder scrolls 6(Beautiful soup)
+'''
+
+import os
+import bs4
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as soup
+
+
