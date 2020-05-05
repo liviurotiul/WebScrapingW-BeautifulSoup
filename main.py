b892ee3f2eec84f8e7f426eb40e663e5c82d1ba9
liviurotiul <liviurotiultelefon@gmail.com>
2020-05-05 03:00:09 +0300

    gets the titles of the reddit threads

--- main.py
+++ main.py
@@ -4,16 +4,24 @@ A quick little web scraping project that notifies if there are any new in the el
 
 import os
 import bs4
-from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import requests
 
 # the page(s) we are gonna be scraping
-site_url = 'https://www.youtube.com/results?search_query=the+elder+scrolls+6&sp=CAMSBAgDEAE%253D'
+site_url = 'https://www.reddit.com/r/ElderScrolls/'
 
-# get the page and read the html
-uClient = uReq(site_url)
-page_html = uClient.read()
-uClient.close()
+# headers to mimic a browser visit
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+# returns a requests.models.Response object
+page_html = requests.get(site_url, headers=headers)
 
 # parse the hmtl
-page_soup = soup(page_html, "html.parser")
+page_soup = soup(page_html.text, "html.parser")
+
+# reddit gets its titles in h3 tags so it's easy to find
+threads = page_soup.findAll('h3')
+for thread in threads:
+	print(thread.text) # get the text out of the tag
+
+
